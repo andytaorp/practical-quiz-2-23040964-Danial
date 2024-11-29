d97a8df91549de247b34e0e4653e8efc4b38d9cd
Danial <23040964@myrp.edu.sg>
2024-11-29 15:30:21 +0800

    adds new component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,37 @@
-import React, { useState } from "react";
-
-export default function Form({ onAddTask }) {
-  const [description, setDescription] = useState("");
-
-  const handleSubmit = (e) => {
-    // TODO: write code to handle form submission
-  };
-
-  return {
-    /*TODO: add a form to add a new task*/
-  };
+import React, { useState } from "react"; 
+ 
+export default function Form({ onAddTask }) { 
+  const [description, setDescription] = useState(""); 
+ 
+  const handleSubmit = (e) => { 
+    e.preventDefault(); 
+ 
+    if (!description) return; 
+ 
+    const newItem = { 
+      id: Date.now(), 
+      description, 
+      packed: false, 
+    }; 
+ 
+    onAddTask(newItem); 
+ 
+    setDescription(""); 
+  }; 
+ 
+  function handleDescription(e) { 
+    setDescription(e.target.value); 
+  } 
+ 
+  return ( 
+    <form onSubmit={handleSubmit}> 
+      <input 
+        type="text" 
+        placeholder="New Task" 
+        value={description} 
+        onChange={handleDescription} 
+      /> 
+      <button type="submit">Add Task</button> 
+    </form> 
+  ); 
 }
